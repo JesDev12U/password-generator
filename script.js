294faf3a2a1c1c3650dd42c6d93379417111663c
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-02-10 12:59:02 -0600

    Término de la programación de la generación de contraseñas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const $body = document.body;
 const $darkLightThemeBtn = document.getElementById("dark-light-theme");
+const $generatePasswordBtn = document.getElementById("generate-password");
 let oscuro = true;
 
 $darkLightThemeBtn.addEventListener("click", (e) => {
@@ -14,3 +15,17 @@ $darkLightThemeBtn.addEventListener("click", (e) => {
     oscuro = true;
   }
 });
+
+const generarPassword = () => {
+  //33 - 122 UTF-8
+  let long = Math.floor(8 + Math.random() * (16 - 8));
+  let pass = "";
+  for (let i = 0; i < long; i++) {
+    pass += String.fromCharCode(Math.floor(33 + Math.random() * (122 - 33)));
+  }
+  const $password = document.getElementById("password-generated");
+  $password.textContent = pass;
+};
+
+window.onload = generarPassword;
+$generatePasswordBtn.addEventListener("click", generarPassword);
